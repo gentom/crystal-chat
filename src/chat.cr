4e2c411ded6db35438fc6f79d80580bae47e047f
gentom <gento.m.developer@gmail.com>
2018-02-09 00:00:07 +0900

    commit

--- src/chat.cr
+++ src/chat.cr
@@ -1,6 +1,14 @@
-require "./chat/*"
+require “kemal”
 
-# TODO: Write documentation for `Chat`
-module Chat
-  # TODO: Put your code here
+SOCKETS = [] of HTTP::WebSocket
+ws “/chat” do |socket|
+  SOCKETS << socket
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message}
+  end
+  socket.on_close do
+    SOCKETS.delete socket
+  end
 end
+
+Kemal.run
